5dae7e06c6ae185829974657d091c3d6adabdee1
01748comforty <muhammadmahdiulislam13@gmail.com>
2025-08-29 21:50:13 +0600

    added Call history function & Btn hover color/background

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Heart counter function
 let totalHeart = 0;
 const total = document.getElementById("total");
 const heart = document.querySelectorAll(".count");
@@ -7,4 +8,41 @@ for (let i = 0; i < heart.length; i++) {
     totalHeart++;
     total.innerText = totalHeart;
   };
+}
+
+
+// Call History function
+let reservedCoins = 100;
+const totalCoin = document.querySelector("#coin-count");
+const callHistory = document.querySelector("#call-history-list");
+
+function makeCall(serviceName, serviceNumber) {
+  if (reservedCoins < 20) {
+    alert("Not enough coins! You need at least 20 coins to make a call.");
+    return;
+  }
+
+  reservedCoins -= 20;
+  totalCoin.innerText = reservedCoins;
+
+  alert(`Calling ${serviceName} at ${serviceNumber}`);
+
+  const currentDate = new Date();
+  const time = currentDate.toLocaleTimeString();
+
+  const newHistory = document.createElement("div");
+  newHistory.className = "history-content flex justify-between items-center p-4 mb-2 bg-[#FAFAFA] rounded-lg";
+
+  newHistory.innerHTML = `
+      <div>
+        <h3 class="ff-inter text-lg font-semibold max-w-[215px] text-[#111] pb-1">${serviceName}</h3>
+        <h4 class="text-lg font-normal text-[#5C5C5C]">${serviceNumber}</h4>
+      </div>
+      <h3 class="time text-lg font-normal text-[#111]">${time}</h3>`;
+
+  callHistory.appendChild(newHistory);
+}
+
+function clearHistory() {
+  callHistory.innerHTML = "";
 }
